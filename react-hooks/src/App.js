133b81d461166de741de149906b59ffa115e1e5d
Mrunal Khodaskar <mrunalikhodaskar@gmail.com>
2024-02-08 13:49:27 +0530

    Revert "Using class Component"
    This reverts commit 2586e9f7ae0eca77dd9f23f8b7072f5ad4ec3726.

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,21 +1,12 @@
 
 import React,{Component} from "react";
 class App extends Component {
-  
-  state = {
+  state ={
     count :0
-  };
-  increment = ()=>{
-    this.setState({
-      count : this.state.count +1
-    });
-  };
+  }
   render(){
   return (
-   <div>
-    <h2>Counter App</h2>
-    <button onClick={this.increment}>Clicked {this.state.count} times</button>
-   </div>
+   <h2>Counter App</h2>
   );
 }
 }
