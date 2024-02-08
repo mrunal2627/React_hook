3ce4eb46a24971dca5063629090ab12b6471174c
Mrunal Khodaskar <mrunalikhodaskar@gmail.com>
2024-02-08 13:35:58 +0530

    Adding React_Hook new project

---
+++ react-hooks/src/App.js
@@ -0,0 +1,14 @@
+
+import React,{Component} from "react";
+class App extends Component {
+  state ={
+    count :0
+  }
+  render(){
+  return (
+   <h2>Counter App</h2>
+  );
+}
+}
+
+export default App;
