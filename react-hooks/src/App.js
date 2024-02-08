2586e9f7ae0eca77dd9f23f8b7072f5ad4ec3726
Mrunal Khodaskar <mrunalikhodaskar@gmail.com>
2024-02-08 13:47:14 +0530

    Using class Component

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,21 @@
 
 import React,{Component} from "react";
 class App extends Component {
-  state ={
+  
+  state = {
     count :0
-  }
+  };
+  increment = ()=>{
+    this.setState({
+      count : this.state.count +1
+    });
+  };
   render(){
   return (
-   <h2>Counter App</h2>
+   <div>
+    <h2>Counter App</h2>
+    <button onClick={this.increment}>Clicked {this.state.count} times</button>
+   </div>
   );
 }
 }
